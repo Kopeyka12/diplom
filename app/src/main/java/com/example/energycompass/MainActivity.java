5b326fc0576b96fa7c56d182c3f32f672c6be11b
Kopeyka12 <miroshin.vladislav@mail.ru>
2024-09-24 09:08:39 +0900

    авторизация

--- app/src/main/java/com/example/energycompass/MainActivity.java
+++ app/src/main/java/com/example/energycompass/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
